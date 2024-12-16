20022fd43c80426dc84411231fe5848e262cb4b6
Wellington Machado de Espindula <wmespindula@inf.ufrgs.br>
2024-12-16 20:04:09 -0300

    Update RTL, contrains + unit tests (prog and ref)

--- sim/rtl/file_list.f
+++ sim/rtl/file_list.f
@@ -1,4 +1,5 @@
 -smartorder -work work -sv -top unit_tests -notimingchecks -gui -access +rw
 ../../rtl/rvsteel_core.v
 ../tb/unit_tests.v
-
+../tb/unit_tests/programs/add-01.hex
+../tb/unit_tests/references/add-01.hex

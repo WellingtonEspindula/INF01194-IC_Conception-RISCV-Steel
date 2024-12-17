811f3605d9c340c33ca95fec48aa0e48dc70ee77
Wellington Machado de Espindula <wmespindula@inf.ufrgs.br>
2024-12-17 11:55:24 -0300

    rename outputs from netlist to rvsteel_core instead of busca_padrao

--- synthesis/physical/6_netlist_sdf.tcl
+++ synthesis/physical/6_netlist_sdf.tcl
@@ -8,9 +8,9 @@
 ##  -Annotate the delay of the final design                 ##
 ##############################################################
 ##Export design netlist
-write_netlist busca_padrao.v
+write_netlist rvsteel_core.v
 ##Annotate design delay
 extract_rc
-write_parasitics -set_load_file busca_padrao.cap
-write_sdf busca_padrao.sdf
+write_parasitics -set_load_file rvsteel_core.cap
+write_sdf rvsteel_core.sdf
 
